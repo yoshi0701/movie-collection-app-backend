7e4193dca5202ca8010dcd18e13c88bcab2d8601
tomoaki yoshioka <tomoaki.yoshiki@gmail.com>
2021-10-23 18:46:37 +0900

    feat: dababase config for movie and get single movie api implemented

--- cmd/api/movie-handlers.go
+++ cmd/api/movie-handlers.go
@@ -1,12 +1,10 @@
 package main
 
 import (
-	"backend/models"
 	"errors"
 	"github.com/julienschmidt/httprouter"
 	"net/http"
 	"strconv"
-	"time"
 )
 
 func (app *application) getOneMovie(w http.ResponseWriter, r *http.Request) {
@@ -21,18 +19,20 @@ func (app *application) getOneMovie(w http.ResponseWriter, r *http.Request) {
 
 	app.logger.Println("id is", id)
 
-	movie := models.Movie{
-		ID:          id,
-		Title:       "some movie",
-		Description: "Some description",
-		Year:        2021,
-		ReleaseDate: time.Date(2021, 01, 01, 01, 0, 0, 0, time.Local),
-		Runtime:     100,
-		Rating:      5,
-		MPAARating:  "PG-13",
-		CreatedAt:   time.Now(),
-		UpdatedAt:   time.Now(),
-	}
+	movie, err := app.models.DB.Get(id)
+
+	//movie := models.Movie{
+	//	ID:          id,
+	//	Title:       "some movie",
+	//	Description: "Some description",
+	//	Year:        2021,
+	//	ReleaseDate: time.Date(2021, 01, 01, 01, 0, 0, 0, time.Local),
+	//	Runtime:     100,
+	//	Rating:      5,
+	//	MPAARating:  "PG-13",
+	//	CreatedAt:   time.Now(),
+	//	UpdatedAt:   time.Now(),
+	//}
 
 	err = app.writeJSON(w, http.StatusOK, movie, "movie")
 }
