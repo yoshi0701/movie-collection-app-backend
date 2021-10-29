2bcf1103033a41fa6ee23f78d98b605d491b5e9e
tomoaki yoshioka <tomoaki.yoshiki@gmail.com>
2021-10-30 00:14:59 +0900

    feat: delete movie api added

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -17,6 +17,7 @@ func (app *application) routes() http.Handler {
 	router.HandlerFunc(http.MethodGet, "/v1/genres", app.getAllGenres)
 
 	router.HandlerFunc(http.MethodPost, "/v1/admin/editmovie", app.editmovie)
+	router.HandlerFunc(http.MethodGet, "/v1/admin/deletemovie", app.deleteMovie)
 
 	return app.enableCORS(router)
 }
