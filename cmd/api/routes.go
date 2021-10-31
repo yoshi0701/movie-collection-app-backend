2064f5b843e74942266467cdd7405c586c73c950
tomoaki yoshioka <tomoaki.yoshiki@gmail.com>
2021-10-31 23:32:22 +0900

    feat: add token check method and secure route implementation

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -1,12 +1,22 @@
 package main
 
 import (
+	"context"
 	"github.com/julienschmidt/httprouter"
+	"github.com/justinas/alice"
 	"net/http"
 )
 
+func (app *application) wrap(next http.Handler) httprouter.Handle {
+	return func(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+		ctx := context.WithValue(r.Context(), "params", ps)
+		next.ServeHTTP(w, r.WithContext(ctx))
+	}
+}
+
 func (app *application) routes() http.Handler {
 	router := httprouter.New()
+	secure := alice.New(app.checkToken)
 
 	router.HandlerFunc(http.MethodGet, "/status", app.statusHandler)
 
@@ -18,7 +28,10 @@ func (app *application) routes() http.Handler {
 
 	router.HandlerFunc(http.MethodGet, "/v1/genres", app.getAllGenres)
 
-	router.HandlerFunc(http.MethodPost, "/v1/admin/editmovie", app.editmovie)
+	// this syntax secure route -> app.wrap(secure.ThenFunc(app.editmovie)))
+	router.POST("/v1/admin/editmovie", app.wrap(secure.ThenFunc(app.editmovie)))
+
+	//router.HandlerFunc(http.MethodPost, "/v1/admin/editmovie", app.editmovie)
 	router.HandlerFunc(http.MethodGet, "/v1/admin/deletemovie/:id", app.deleteMovie)
 
 	return app.enableCORS(router)
